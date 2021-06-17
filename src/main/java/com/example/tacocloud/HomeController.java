ec9e57c700d1a11d5701e37a9ecb8f78a8818f2f
Sylwinn Tudor <swtudor@gmail.com>
2021-06-16 21:07:50 -0500

    day 1

---
+++ src/main/java/com/example/tacocloud/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/taco")
+    public String returnJson(){
+        return "taco";
+    }
+
+
+}
