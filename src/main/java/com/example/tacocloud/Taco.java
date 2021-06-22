6c557ef9f84cca85aa514078c4ab39ace60abfd4
Tudor <studor@stlcc.edu>
2021-06-22 15:44:09 -0500

    day 3 in-class work. not catching errors

--- src/main/java/com/example/tacocloud/Taco.java
+++ src/main/java/com/example/tacocloud/Taco.java
@@ -1,11 +1,16 @@
 package com.example.tacocloud;
 
 import lombok.Data;
-import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min=3, message = "Name must be at least 3 characters long")
     private String name;
+    @Size(min=2, message="You must have at least 2 ingredients to make a taco")
     private List<String> ingredients;
 }
